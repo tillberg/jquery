3d86732a3503b791de771aa5a4cac51898807754
Dan Tillberg <dan@tillberg.us>
2009-11-21 23:11:31 -0500

    Completely reworked the prototyping stuff into $.proto

--- src/proto.js
+++ src/proto.js
@@ -1,23 +1,34 @@
 
-// Douglas Crockford's object branching method
-Object.create = function(o) {
-  function F() { }
-  F.prototype = o;
-  return new F();
-};
+jQuery.extend({
 
-// Create a new prototype with the specified name, optionally deriving from an existing prototype
-function PROTO(o) {
-  var p = Object.create(o || {});
-  p.is = function(s) {return s === p || (o && o.is(s)); };
-  return p;
-}
+    // Create a new prototype with the specified name, optionally deriving from an existing prototype
+    proto: function( par ) {
+		
+		// Adapted form of Douglas Crockford's Object.create
+		function create( z ) {
+			function F() { }
+		  	F.prototype = z;
+		  	return new F();
+		}
+		
+		// Make a new prototype based on the specified parent prototype, if any
+        var p = create( par || {} );
+		
+		// Recursively determine if the specified object is of the specified prototype
+        p.is = function( s ) {
+			return s === p || ( par && par.is( s ) ); 
+		};
+		
+		p.new = function( ) {
+  			var o = create( p );
+			// Execute the top-most constructor, passing in the specified arguments
+  			if ( p.cons ) {
+				p.cons.apply( o, arguments ); 
+			}
+  			return o;
+		};
+		
+        return p;
+    }
 
-// Create a new object from proto, calling constructor
-function NEW(p) {
-  var o = Object.create(p);
-  if (o.cons) { o.cons.apply(o, [].slice.call(arguments, 1)); }
-  return o;
-}
-
-
+});
