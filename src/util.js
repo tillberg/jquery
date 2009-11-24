a8f2e1c0d1c6ce3e586630786bc3452cbdc3d32b
Dan Tillberg <dan@tillberg.us>
2009-11-24 09:07:20 -0500

    Removing some stuff from util that we've already implemented

--- src/util.js
+++ src/util.js
@@ -1,25 +1,4 @@
 
-// Douglas Crockford's object branching method
-Object.create = function(o) {
-  function F() { }
-  F.prototype = o;
-  return new F();
-};
-
-// Create a new prototype with the specified name, optionally deriving from an existing prototype
-function PROTO(o) {
-  var p = Object.create(o || {});
-  p.is = function(s) {return s === p || (o && o.is(s)); };
-  return p;
-}
-
-// Create a new object from proto, calling constructor
-function NEW(p) {
-  var o = Object.create(p);
-  if (o.cons) { o.cons.apply(o, [].slice.call(arguments, 1)); }
-  return o;
-}
-
 function isdef(x) { return x !== undefined; }
 function isstr(x) { return (("" + typeof x) == "string"); }
 function isobj(x) { return (("" + typeof x) == "object"); }
