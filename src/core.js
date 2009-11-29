f1ee7101ea0953bbcebe73d33c8c4eaf9eed262f
Dan Tillberg <dan@tillberg.us>
2009-11-29 09:32:19 -0500

    Moved most extended stuff to corex.js (to minimize mucking with and size of core.js)

--- src/core.js
+++ src/core.js
@@ -159,12 +159,11 @@ jQuery.fn = jQuery.prototype = {
 	// The default length of a jQuery object is 0
 	length: 0,
 
-	// Return true if the specified key is a user property defined on this object
-	// e.g. ok($({name: 'Bob'}).isUserKey('name'), ...)
-	isUserKey: function(k) {
-		return this.o && (this.o[k] !== undefined);
+	// The number of elements contained in the matched element set
+	size: function() {
+		return this.length;
 	},
-	
+
 	toArray: function(){
 		return slice.call( this.o ? this.vals() : this, 0 );
 	},
@@ -180,7 +179,7 @@ jQuery.fn = jQuery.prototype = {
 			// Return just the object
 			( num < 0 ? this.slice(num)[ 0 ] : this[ num ] );
 	},
-	
+
 	// Take an array of elements and push it onto the stack
 	// (returning the new matched element set)
 	pushStack: function( elems, name, selector ) {
@@ -220,52 +219,7 @@ jQuery.fn = jQuery.prototype = {
 	each: function( callback, args ) {
 		return jQuery.each( this.o || this, callback, args );
 	},
-	
-	grep: function( a, b ) {
-		return jQuery( jQuery.grep( this.o || this, a, b, this.o ) );
-	},
-	
-	mapToObj: function( cb ) {
-		return jQuery( jQuery.mapToObj( this.o || this, cb ) );
-	},
-	
-	peek: function() {
-		return this[ this.length - 1 ];
-	},
-	
-	keys: function() {
-		var ret = [ ];
-		for ( var k in this.raw() ) {
-			ret.push( k );
-		}
-		return jQuery( ret );
-	},
-	
-	vals: function() {
-		var ret = [], raw = this.raw();
-		
-		// This would speed up calls of this function when holding an array
-		//if ( !this.o ) { return raw; }
-		
-		for ( var k in raw ) {
-			ret.push( raw[ k ] );
-		}
-		return jQuery( ret );
-	},
-	
-	join: function( sep ) {
-		// Join the values of either the object or array with the specified separator
-		return [].join.call( this.o ? this.vals() : this, sep );
-	},
 
-	raw: function() {
-		return this.o || this.toArray();
-	},
-	
-	toJson: function() {
-		return JSON.stringify( this.raw() );
-	},
-	
 	// Determine the position of an element within
 	// the matched set of elements
 	index: function( elem ) {
@@ -284,26 +238,12 @@ jQuery.fn = jQuery.prototype = {
 	is: function( selector ) {
 		return !!selector && jQuery.filter( selector, this ).length > 0;
 	},
-	
-	reverse: function() {
-		[].reverse.call( this );
-		return this;
-	},
-	
-	reversed: function() {
-		return jQuery( this ).reverse();
-	},
-	
-	random: function() {
-		return this.eq( jQuery.randInt( this.length ) );
-	},
-	
+
 	// For internal use only.
 	// Behaves like an Array's method, not like a jQuery method.
 	push: push,
 	sort: [].sort,
-	splice: [].splice,
-	pop: [].pop
+	splice: [].splice
 };
 
 // Give the init function the jQuery prototype for later instantiation
@@ -365,26 +305,6 @@ jQuery.extend = jQuery.fn.extend = function() {
 	return target;
 };
 
-
-// Find the nth value in a.  If y, then return the nth key instead.
-// If n is negative, return the last key/value for objects, or the
-// mod-indexed key/value for arrays.
-function nth( a, n, y ) {
-	var l = a.length, v;
-	if ( l !== undefined ) { 
-		if ( n < 0 ) { n = (n + l) % l; }
-		// Some parentheses would be nice here... but they cost bytes
-		return y ? l > 0 ? n : undefined : a[ n ];
-	}
-	
-	for ( var k in a ) {
-		v = y ? k : a[ k ];
-		if (n === 0) { return v; }
-		n--;
-	}
-	return v;
-}
-
 jQuery.extend({
 	noConflict: function( deep ) {
 		window.$ = _$;
@@ -395,7 +315,7 @@ jQuery.extend({
 
 		return jQuery;
 	},
-	
+
 	// See test/unit/core.js for details concerning isFunction.
 	// Since version 1.3, DOM methods and functions like alert
 	// aren't supported. They return false on IE (#2968).
@@ -427,29 +347,12 @@ jQuery.extend({
 		
 		return key === undefined || hasOwnProperty.call( obj, key );
 	},
-	
-    // Is a given array or object empty?
-	isEmpty: function( obj ) {
+
+	isEmptyObject: function( obj ) {
 		for ( var name in obj ) {
 			return false;
 		}
 		return true;
-	},
-
-    // Is a given value a DOM element?
-	isElement: function( obj ) {
-		return !!(obj && obj.nodeType == 1);
-	},
-
-	// Is a given variable defined?
-	isDefined: function( obj ) {
-		return typeof obj != 'undefined';
-	},
-	
-	// The original jQuery function to test for emptiness.
-	// Just call the underscore version.
-	isEmptyObject: function( obj ) {
-		return jQuery.isEmpty( obj );
 	},
 
 	// check if an element is in a (or is an) XML document
@@ -489,7 +392,7 @@ jQuery.extend({
 
 	// args is for internal usage only
 	each: function( object, callback, args ) {
-	    var name, i = 0,
+		var name, i = 0,
 			length = object.length,
 			isObj = length === undefined || jQuery.isFunction(object);
 
@@ -598,15 +501,6 @@ jQuery.extend({
 
 		return ret;
 	},
-	
-	// Filter b, returning k/v pairs that are different from those in a
-	grepDiff: function( a, b ) {
-		var c = {};
-		jQuery.each(b, function( k, v ) {
-			if ( !a || a[ k ] !== v ) { c[ k ] = v; }
-		});
-		return c;
-	},
 
 	map: function( elems, callback ) {
 		var ret = [], value;
@@ -633,78 +527,7 @@ jQuery.extend({
 
 		return ret.concat.apply( [], ret );
 	},
-	
-	mapToObj: function( o, cb ) {
-		var r = {};
-		jQuery.each( o, function( k, v ) {
-			var x = cb( v, k );
-			if ( jQuery.isArray( x ) ) {
-				// Return value was of form [ k, v ]
-				r[ x[ 0 ] ] = x[ 1 ];
-			} else {
-				// Return value was of form { k1: v1, ... }
-				jQuery.extend( r, x );
-			}
-		});
-		return r;
-	},
-	
-	front: function( a ) {
-		return nth( a, 0 );
-	},
-	
-	frontKey: function( a ) {
-		return nth( a, 0, 1 );
-	},
-	
-	back: function( a ) {
-		return nth( a, -1 );
-	},
-	
-	backKey: function( a ) {
-		return nth( a, -1, 1 );
-	},
-	
-	// Return an array of integers from 0 to m - 1, or m to n - 1 if n is specified.
-    range: function( m, n ) {
-    	if (n === undefined) { n = m; m = 0; }
-      	var r = [];
-      	for ( var i = m; i < n; i++ ) { r.push( i ); }
-      	return jQuery( r );
-    },
-    
-	// Return a filtered copy of o, including only the keys in fields
-	filterByKey: function( o, fields ) {
-		var r = {};
-	  	jQuery.each( fields, function( i, f ) {
-	    	r[ f ] = o[ f ];
-	    });
-		return r;
-	},
-	
-	// The number of elements contained in the matched element set
-	// If cb is specified it is used as a test function
-	size: function( o, cb ) {
-		// If this is a normal jQuery object, return the array length
-		if ( !cb && o.length !== undefined ) { return o.length; }
-		
-		// If this holds an object literal, count the number of keys
-		var n = 0;
-		jQuery.each( o, function( k, v ) {
-			if ( !cb || cb( v, k ) ) { n++; }
-		});
-		return n;
-	},
-	
-	// Generate a random number from 0 to l or l to h, exclusive.
-	randInt: function( l, h ) {
-		if ( h === undefined ) {
-			h = l;
-			l = 0;
-		}
-		return Math.floor( Math.random() * (h - l) ) + l;
-	},
-	
+
 	// Use of jQuery.browser is deprecated.
 	// It's included for backwards compatibility and plugins,
 	// although they should work to migrate away.
@@ -715,14 +538,6 @@ jQuery.extend({
 		msie: /msie/.test( userAgent ) && !/opera/.test( userAgent ),
 		mozilla: /mozilla/.test( userAgent ) && !/(compatible|webkit)/.test( userAgent )
 	}
-});
-
-// Attach inline versions of simple object/array methods to jQuery.fn
-// This is more compact than defining them inline above
-jQuery.map( 'front,frontKey,back,backKey,filterByKey,size,grepDiff'.split( ',' ), function( x ) {
-	jQuery.fn[ x ] = function( a ) { 
-		return jQuery[ x ]( this.o || this, a );
-	};
 });
 
 if ( indexOf ) {
---
+++ src/corex.js
@@ -0,0 +1,192 @@
+
+
+jQuery.fn.extend({
+	
+	// Is a given value a DOM element?
+	isElement: function( obj ) {
+		return !!(obj && obj.nodeType == 1);
+	},
+
+	// Is a given variable defined?
+	isDefined: function( obj ) {
+		return typeof obj != 'undefined';
+	},
+	
+    // Return true if the specified key is a user property defined on this object
+	// e.g. ok($({name: 'Bob'}).isUserKey('name'), ...)
+	isUserKey: function(k) {
+		return this.o && (this.o[k] !== undefined);
+	},
+	
+	grep: function( a, b ) {
+		return jQuery( jQuery.grep( this.o || this, a, b, this.o ) );
+	},
+	
+	mapToObj: function( cb ) {
+		return jQuery( jQuery.mapToObj( this.o || this, cb ) );
+	},
+	
+	peek: function() {
+		return this[ this.length - 1 ];
+	},
+	
+	keys: function() {
+		var ret = [ ];
+		for ( var k in this.raw() ) {
+			ret.push( k );
+		}
+		return jQuery( ret );
+	},
+	
+	vals: function() {
+		var ret = [], raw = this.raw();
+		
+		// This would speed up calls of this function when holding an array
+		//if ( !this.o ) { return raw; }
+		
+		for ( var k in raw ) {
+			ret.push( raw[ k ] );
+		}
+		return jQuery( ret );
+	},
+	
+	join: function( sep ) {
+		// Join the values of either the object or array with the specified separator
+		return [].join.call( this.o ? this.vals() : this, sep );
+	},
+
+	raw: function() {
+		return this.o || this.toArray();
+	},
+	
+	toJson: function() {
+		return JSON.stringify( this.raw() );
+	},
+	
+	reverse: function() {
+		[].reverse.call( this );
+		return this;
+	},
+	
+	reversed: function() {
+		return jQuery( this ).reverse();
+	},
+	
+	random: function() {
+		return this.eq( jQuery.randInt( this.length ) );
+	},
+	
+	pop: [].pop
+});
+
+// Find the nth value in a.  If y, then return the nth key instead.
+// If n is negative, return the last key/value for objects, or the
+// mod-indexed key/value for arrays.
+function nth( a, n, y ) {
+	var l = a.length, v;
+	if ( l !== undefined ) { 
+		if ( n < 0 ) { n = (n + l) % l; }
+		// Some parentheses would be nice here... but they cost bytes
+		return y ? l > 0 ? n : undefined : a[ n ];
+	}
+	
+	for ( var k in a ) {
+		v = y ? k : a[ k ];
+		if (n === 0) { return v; }
+		n--;
+	}
+	return v;
+}
+
+jQuery.extend({
+	
+	// Filter b, returning k/v pairs that are different from those in a
+	grepDiff: function( a, b ) {
+		var c = {};
+		jQuery.each(b, function( k, v ) {
+			if ( !a || a[ k ] !== v ) { c[ k ] = v; }
+		});
+		return c;
+	},
+
+	mapToObj: function( o, cb ) {
+		var r = {};
+		jQuery.each( o, function( k, v ) {
+			var x = cb( v, k );
+			if ( jQuery.isArray( x ) ) {
+				// Return value was of form [ k, v ]
+				r[ x[ 0 ] ] = x[ 1 ];
+			} else {
+				// Return value was of form { k1: v1, ... }
+				jQuery.extend( r, x );
+			}
+		});
+		return r;
+	},
+	
+	front: function( a ) {
+		return nth( a, 0 );
+	},
+	
+	frontKey: function( a ) {
+		return nth( a, 0, 1 );
+	},
+	
+	back: function( a ) {
+		return nth( a, -1 );
+	},
+	
+	backKey: function( a ) {
+		return nth( a, -1, 1 );
+	},
+	
+	// Return an array of integers from 0 to m - 1, or m to n - 1 if n is specified.
+    range: function( m, n ) {
+    	if (n === undefined) { n = m; m = 0; }
+      	var r = [];
+      	for ( var i = m; i < n; i++ ) { r.push( i ); }
+      	return jQuery( r );
+    },
+    
+	// Return a filtered copy of o, including only the keys in fields
+	filterByKey: function( o, fields ) {
+		var r = {};
+	  	jQuery.each( fields, function( i, f ) {
+	    	r[ f ] = o[ f ];
+	    });
+		return r;
+	},
+	
+	// The number of elements contained in the matched element set
+	// If cb is specified it is used as a test function
+	size: function( o, cb ) {
+		// If this is a normal jQuery object, return the array length
+		if ( !cb && o.length !== undefined ) { return o.length; }
+		
+		// If this holds an object literal, count the number of keys
+		var n = 0;
+		jQuery.each( o, function( k, v ) {
+			if ( !cb || cb( v, k ) ) { n++; }
+		});
+		return n;
+	},
+	
+	// Generate a random number from 0 to l or l to h, exclusive.
+	randInt: function( l, h ) {
+		if ( h === undefined ) {
+			h = l;
+			l = 0;
+		}
+		return Math.floor( Math.random() * (h - l) ) + l;
+	}
+	
+});
+
+// Attach inline versions of simple object/array methods to jQuery.fn
+// This is more compact than defining them inline above
+// size will actually overwrite the default jQuery version
+jQuery.map( 'front,frontKey,back,backKey,filterByKey,size,grepDiff'.split( ',' ), function( x ) {
+	jQuery.fn[ x ] = function( a ) { 
+		return jQuery[ x ]( this.o || this, a );
+	};
+});
